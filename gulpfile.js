3963a8eb62d41787729ca51bb61d21ff8c7c3e8d
summerangel <summerlisi@163.com>
2016-10-21 17:32:42 +0800

    first add project file

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+/**
+ * Created by summer on 16/10/21.
+ */
+
+
+//引入组件
+var gulp = require('gulp');
+
+//引入组件
+var jshint = require('gulp-jshint'); //检查js
+var sass = require('gulp-sass'); //编译sass
+var concat = require('gulp-concat'); //合并
+var uglify = require('gulp-uglify'); //uglify 组件（用于压缩js）
+var rename = require('gulp-rename'); //重命名
+
+//检查js脚本的任务
+gulp.task('lint',function(){
+   gulp.src('src/js/*.js')   //可配置你需要检查脚本的具体名字
+       .pipe(jshint())
+       .pipe(jshint.reporter('default'));
+});
+
+//编译sass
+gulp.task('sass',function(){
+    gulp.src('src/sass/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./css')); //dest()写入文件
+});
+
+//合并，压缩Js文件
+//找到js/目录下的所有js文件
+gulp.task('scripts',function () {
+    gulp.src('src/js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('./dist'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist'));
+    console.log('gulp task is done')
+});
+
+
+//定义默认任务，执行gulp会自动执行的任务
+gulp.task('default',function(){
+   gulp.run('lint','sass','scripts');
+
+    //监听js文件变化，当文件发生变化后会自动执行任务
+    gulp.watch('src/js/*.js',function(){
+       gulp.run('lint','scripts');
+    });
+
+});
---
+++ src/js/test.js
@@ -0,0 +1,6 @@
+/**
+ * Created by summer on 16/10/21.
+ */
+var a='I am just for a gulp test,the first time;';
+
+console.log(a);
